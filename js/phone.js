4437c232b8f7b9e2777f153d256b0c337fa92444
rehanulhb <rehanulh@gmail.com>
2024-10-16 02:47:28 +0200

    Search Phones using Keyword from Input Text field

--- js/phone.js
+++ js/phone.js
@@ -1,5 +1,5 @@
-const loadPhone = async () =>{
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+const loadPhone = async (searchText) =>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
     //console.log(phones);
@@ -10,13 +10,16 @@ const displayPhones = phones =>{
 
     const phoneContainer = document.getElementById('phone-container');
 
+    //Clear Phone Container cards before adding new Cards
+    phoneContainer.textContent ='';
+
     //console.log(phones);
     phones.forEach(phone=>{
-        console.log(phone);
+        //console.log(phone);
 
         //2 Create a Div
         const phoneCard = document.createElement('div');
-        phoneCard.classList = `card bg-gray-100 w-96 shadow-xl`;
+        phoneCard.classList = `card bg-gray-100 p-4 shadow-xl`;
         // 3 Set Inner HTML
         phoneCard.innerHTML = `
         <figure>
@@ -39,4 +42,13 @@ const displayPhones = phones =>{
     })
 }
 
-loadPhone();
+
+//Search Button
+const handleSearch = () =>{
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    console.log(searchText);
+    loadPhone(searchText);
+}
+
+//loadPhone();
