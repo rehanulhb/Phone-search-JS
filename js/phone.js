edd05a259091a06d6232dd179427207acd391cc8
rehanulhb <rehanulh@gmail.com>
2024-10-17 23:51:38 +0200

    Added Show Modal Options but didnt implement it

--- js/phone.js
+++ js/phone.js
@@ -47,8 +47,8 @@ const displayPhones = (phones,isShowAll) =>{
                     <div class="card-body">
                       <h2 class="card-title">${phone.phone_name}</h2>
                       <p>If a dog chews shoes whose shoes does he choose?</p>
-                      <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Buy Now</button>
+                      <div class="card-actions justify-center">
+                        <button onclick="handleShowDetail('${phone.slug}')" class="btn btn-primary">Show Details</button>
                       </div>
                     </div>
         `;
@@ -86,6 +86,18 @@ const toggleLoadingSpinner = (isLoading) => {
 //Handle show all
 const handleShowAll = () =>{
     handleSearch(true);
+}
+
+const handleShowDetail = async(id) =>{
+    console.log('Clicked Show Details',id);
+    
+    //Load Single Phone Data
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data = await res.json();
+    console.log(data);
+
+
+
 }
 
 
